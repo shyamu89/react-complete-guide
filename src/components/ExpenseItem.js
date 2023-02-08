23691bf6144405bbed0bffe431a28ccf7b5438b5
shyamu89 <shyamal.clawsys@gmail.com>
2023-02-08 06:29:17 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expenseDate = new Date(2023, 2, 2);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = "293.97";
+
+  return (
+    <div className="expense-item">
+      <div className="expense-item__date">{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+      </div>
+      <div className="expense-item__price">${expenseAmount}</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
