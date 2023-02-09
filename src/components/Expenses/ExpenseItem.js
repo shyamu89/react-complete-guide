a6d4618d93a71301b4f9e4e76efe2983f4bb4d08
shyamu89 <shyamal.clawsys@gmail.com>
2023-02-09 13:19:41 +0530

    48. Working with "State"

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 //43. An Alternative Function Syntax, e.g: const App = () => {
 const ExpenseItem = (props) => {
+  const [title, seTitle] = useState(props.title);
   const clickHandler = () => {
-    console.log("Clicked");
+    seTitle("Updated!");
+    console.log(title);
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
       {/* <button
