0cf001224096d72ace7ff07c387f4c8caa071c95
shyamu89 <shyamal.clawsys@gmail.com>
2023-02-10 13:02:04 +0530

    52. Listening to User Input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 //43. An Alternative Function Syntax
@@ -25,19 +26,19 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "Let's get started!"),
-    React.createElement(Expenses, { items: expenses })
-  );
-
-  // return (
-  //   <div>
-  //     <h2>Let's get started!</h2>
-  //     <Expenses items={expenses} />
-  //   </div>
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
   // );
+
+  return (
+    <div>
+      <NewExpense />
+      <Expenses items={expenses} />
+    </div>
+  );
 };
 
 export default App;
