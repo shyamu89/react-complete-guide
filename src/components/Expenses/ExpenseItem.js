fcb55c32be7ff6028ccd22c35afd49ea63bdeee8
shyamu89 <shyamal.clawsys@gmail.com>
2023-02-11 18:28:40 +0530

    60. Controlled vs Uncontrolled Components & Stateless vs Stateful Components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,31 +1,34 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 //43. An Alternative Function Syntax, e.g: const App = () => {
 const ExpenseItem = (props) => {
-  const [title, seTitle] = useState(props.title);
-  console.log("expenseItem evaluated by React");
-  const clickHandler = () => {
-    seTitle("Updated!");
-    console.log(title);
-  };
+  // const [title, seTitle] = useState(props.title);
+  // console.log("expenseItem evaluated by React");
+  // const clickHandler = () => {
+  //   seTitle("Updated!");
+  //   console.log(title);
+  // };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
-      </div>
-      <div className="expense-item__price">${props.amount}</div>
-      {/* <button
+        {/* <h2>{title}</h2> */}
+        <h2>{props.title}</h2>
+
+        <div className="expense-item__price">${props.amount}</div>
+        {/* <button
         onClick={() => {
           console.log("Clicked");
         }}
       >
         Change Title
       </button> */}
-      <button onClick={clickHandler}>Change Title</button>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
+      </div>
     </Card>
   );
 };
